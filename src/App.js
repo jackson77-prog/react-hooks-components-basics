f130d1115cd67066885cf076470c173a99e70fa5
jackson77-prog <jackson.njihia@student.moringaschool.com>
2024-03-07 03:35:55 +0300

    added Comment2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Comment2 from "./Comment2"
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Comment2 />
     </div>
   );
 }
---
+++ src/Comment2.js
@@ -0,0 +1,3 @@
+import React from "react";
+const Comment2= () => <div> I beg to differ, word perfect is ideal!</div>;
+export default Comment2;
